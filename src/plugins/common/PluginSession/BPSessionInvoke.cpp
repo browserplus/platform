ff064ee8aa60f65c18a0a78c7285c61089195d2b
dgrigsby <dgrigsby@3fcf768e-b076-0410-b9b8-e8c34e2d470b>
2010-01-26 03:03:40 +0000

    Log service method invocation arguments at debug level.
    git-svn-id: svn+ssh://corpsvnproxy/yahoo/BrowserPlus/public_platform/trunk@120 3fcf768e-b076-0410-b9b8-e8c34e2d470b

--- src/plugins/common/PluginSession/BPSessionInvoke.cpp
+++ src/plugins/common/PluginSession/BPSessionInvoke.cpp
@@ -214,7 +214,9 @@ BPSession::executeMethod(const std::string &service,
 
     args = dynamic_cast<bp::Map *>(argsObj);
     argsObj = NULL;
-    
+
+    BPLOG_DEBUG_STRM(service << "[" << version << "]." << method <<
+                     "(" << args->toPlainJsonString(true) << ")");
 
     // allocate an execution context ptr.
     ExecuteContext * ctx = new ExecuteContext;
