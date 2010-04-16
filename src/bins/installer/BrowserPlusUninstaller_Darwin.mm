de29050e5428f0b45f8e15e27156c796c323969f
durand <durand@3fcf768e-b076-0410-b9b8-e8c34e2d470b>
2010-04-16 18:38:20 +0000

    {#155} Add confirm/status dialogs for osx uninstall. Also added -quiet flag, which bypasses dialogs.
    git-svn-id: svn+ssh://corpsvnproxy/yahoo/BrowserPlus/public_platform/trunk@419 3fcf768e-b076-0410-b9b8-e8c34e2d470b

---
+++ src/bins/installer/BrowserPlusUninstaller_Darwin.mm
@@ -0,0 +1,134 @@
+/**
+ * ***** BEGIN LICENSE BLOCK *****
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is BrowserPlus (tm).
+ * 
+ * The Initial Developer of the Original Code is Yahoo!.
+ * Portions created by Yahoo! are Copyright (c) 2009 Yahoo! Inc.
+ * All rights reserved.
+ * 
+ * Contributor(s): 
+ * ***** END LICENSE BLOCK *****
+ */
+
+
+#include "BPUtils/ARGVConverter.h"
+#include "BPUtils/bpfile.h"
+#include "BPUtils/BPLog.h"
+#include "BPUtils/bpexitcodes.h"
+#include "BPUtils/bpexitcodes.h"
+#include "BPUtils/bplocalization.h"
+#include "BPUtils/bpstrutil.h"
+#include "BPInstaller/BPInstaller.h"
+
+#import <Cocoa/Cocoa.h>
+
+using namespace std;
+using namespace bp::file;
+using namespace bp::localization;
+
+int
+main(int argc, const char** argv)
+{
+    try {
+        // debug logging on be default
+        Path logFile = getTempDirectory() / "BrowserPlusUninstaller.log";
+        string logLevel = "debug";
+
+        bool quiet = false;
+        vector<string> args;
+        for (int i = 1; i < argc; i++) {
+            if (!strncmp(argv[i], "-quiet", 6)) {
+                quiet = true;
+                continue;
+            }
+
+            // skip args starting with -psn which deliver the "Process Serial
+            // Number" and are added by the OSX launcher
+            if (!strncmp(argv[i], "-psn", 4)) continue;
+
+            args = bp::strutil::split(argv[i], "=");
+            if (!args[0].compare("-logfile")) {
+                if (!args[1].compare("console")) {
+                    logFile.clear();
+                } else {
+                    logFile = args[1];
+                }
+            } else if (!args[0].compare("-log")) {
+                logLevel = args[1];
+            }
+        }
+    
+        if (!logLevel.empty()) {
+            if (logFile.empty()) {
+                bp::log::setupLogToConsole(logLevel);
+            } else {
+                bp::log::setupLogToFile(logFile, logLevel,
+                                        bp::log::kSizeRollover);
+            }
+        }
+
+        // gunk we may need for alert
+        NSApplication* application = nil;
+        NSAutoreleasePool* pool = nil;
+
+        // perhaps prompt user for confirmation
+        string locale, prompt, done, yes, no, ok;
+        if (!quiet) {
+            application = [NSApplication sharedApplication];
+            pool = [[NSAutoreleasePool alloc] init];
+
+            // Get the strings we need now, in case we need them after deleting
+            // resource files.
+            const string locale = getUsersLocale();
+            getLocalizedString("uninstallPrompt", locale, prompt);
+            getLocalizedString("uninstallNotification", locale, done);
+            getLocalizedString("yes", locale, yes);
+            getLocalizedString("no", locale, no);
+            getLocalizedString("ok", locale, ok);
+
+            NSAlert* alert = [[NSAlert alloc] init];
+            [alert setAlertStyle: NSCriticalAlertStyle];
+            [alert setMessageText: [NSString stringWithUTF8String: prompt.c_str()]];
+            [alert addButtonWithTitle: [NSString stringWithUTF8String: yes.c_str()]];
+            [alert addButtonWithTitle: [NSString stringWithUTF8String: no.c_str()]];
+
+            int ret = [alert runModal];
+            [alert release];
+            if (ret == NSAlertSecondButtonReturn) {
+                // user canceled
+                BPLOG_INFO( "User cancelled uninstall" );
+                return bp::exit::kUserCancel;
+            }
+        }
+
+        // Actually do the uninstall
+        bp::install::Uninstaller unins;
+        unins.run();
+
+        // perhaps let user know we're done
+        if (!quiet) {
+            NSAlert* alert = [[NSAlert alloc] init];
+            [alert setAlertStyle: NSInformationalAlertStyle];
+            [alert setMessageText: [NSString stringWithUTF8String: done.c_str()]];
+            [alert addButtonWithTitle: [NSString stringWithUTF8String: ok.c_str()]];
+            (void) [alert runModal];
+            [alert release];
+            [pool release];
+        }
+
+    } catch (const bp::error::Exception& e) {
+        BPLOG_ERROR_STRM("Uninstall failed: " << e.what());
+    } catch (const bp::error::FatalException& e) {
+        BPLOG_ERROR_STRM("Uninstall failed: " << e.what());
+    }
+}
