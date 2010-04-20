280f0df99f15ae50de9ddc4768cf2cafb274029a
lloydh <lloydh@3fcf768e-b076-0410-b9b8-e8c34e2d470b>
2010-04-20 00:34:18 +0000

    Fix copy/paste bug with permanently denying autoupdate perms.
    git-svn-id: svn+ssh://corpsvnproxy/yahoo/BrowserPlus/public_platform/branches/2.7@449 3fcf768e-b076-0410-b9b8-e8c34e2d470b

--- src/libs/permissions/PermissionsManager.cpp
+++ src/libs/permissions/PermissionsManager.cpp
@@ -858,7 +858,7 @@ PermissionsManager::saveDomainPermissions()
                             sm->add(svcIter->first, new bp::Bool(true));
                             break;
                         case eNotAllowed:
-                            dm->add(svcIter->first, new bp::Bool(false));
+                            sm->add(svcIter->first, new bp::Bool(false));
                             break;
                         case eUnknown:
                             break;
