a5f2b3f79c31460a9bd1ba6fdd925d7da7d1ac79
lloydh <lloydh@3fcf768e-b076-0410-b9b8-e8c34e2d470b>
2010-05-19 00:43:45 +0000

    update all source licenses
    git-svn-id: svn+ssh://corpsvnproxy/yahoo/BrowserPlus/public_platform/trunk@566 3fcf768e-b076-0410-b9b8-e8c34e2d470b

--- src/support/build_scripts/BuildSDK.cmake
+++ src/support/build_scripts/BuildSDK.cmake
@@ -12,7 +12,7 @@
 # The Original Code is BrowserPlus (tm).
 # 
 # The Initial Developer of the Original Code is Yahoo!.
-# Portions created by Yahoo! are Copyright (c) 2009 Yahoo! Inc.
+# Portions created by Yahoo! are Copyright (c) 2010 Yahoo! Inc.
 # All rights reserved.
 # 
 # Contributor(s): 
