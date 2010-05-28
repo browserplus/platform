1b6c95ede8cb72e7b732071c335149ce5bb37acd
lloydh <lloydh@3fcf768e-b076-0410-b9b8-e8c34e2d470b>
2010-05-28 05:27:14 +0000

    coversion of v4 types to v5 types
    git-svn-id: svn+ssh://corpsvnproxy/yahoo/BrowserPlus/public_platform/branches/service_api_v5@611 3fcf768e-b076-0410-b9b8-e8c34e2d470b

--- src/libs/service_runner/Process/v4/V4ObjectConverter.cpp
+++ src/libs/service_runner/Process/v4/V4ObjectConverter.cpp
@@ -27,11 +27,61 @@
 
 
 #include "V4ObjectConverter.h"
+#include "BPUtils/bptypeutil.h"
 
 bp::Object *
 sapi_v4::v4ToBPObject(const struct sapi_v4::BPElement_t * elemPtr)
 {
-    return NULL;
+    if (!elemPtr) return NULL;
+    bp::Object * o = NULL;
+    
+    switch (elemPtr->type) {
+        case BPTNull:
+            o = new bp::Null;
+            break;
+        case BPTBoolean:
+            o = new bp::Bool(elemPtr->value.booleanVal);
+            break;
+        case BPTInteger:
+            o = new bp::Integer(elemPtr->value.integerVal);
+            break;
+        case BPTDouble:
+            o = new bp::Double(elemPtr->value.doubleVal);
+            break;
+        case BPTString:
+            o = new bp::String(elemPtr->value.stringVal);
+            break;
+        case BPTMap: {
+            bp::Map * m = new bp::Map;
+            for (unsigned int i = 0; i < elemPtr->value.mapVal.size; i++) {
+                m->add(elemPtr->value.mapVal.elements[i].key,
+                       v4ToBPObject(elemPtr->value.mapVal.elements[i].value));
+            }
+            o = m;
+            break;
+        }
+        case BPTList: {
+            bp::List * l = new bp::List;
+            for (unsigned int i = 0; i < elemPtr->value.listVal.size; i++)
+            {
+                l->append(v4ToBPObject(elemPtr->value.listVal.elements[i]));
+            }
+            o = l;
+            break;
+        }
+        case BPTCallBack:
+            o = new bp::CallBack(elemPtr->value.callbackVal);
+            break;
+        case BPTPath:
+            // AHA: a v4 Path is a utf8 encoded url, here we convert. 
+            o = new bp::Path(bp::file::pathFromURL(elemPtr->value.pathVal));
+            break;
+        case BPTAny:
+            // noop!
+            break;
+    }
+
+    return o;
 }
 
 sapi_v4::BPElement *
