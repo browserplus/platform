18cdf8d82cf70e8e877eb5bf9641f74bbcd022b0
lloydh <lloydh@3fcf768e-b076-0410-b9b8-e8c34e2d470b>
2010-06-02 04:23:02 +0000

    bump trunk to 2.9.1 after tagging 2.9.0
    git-svn-id: svn+ssh://corpsvnproxy/yahoo/BrowserPlus/public_platform/trunk@636 3fcf768e-b076-0410-b9b8-e8c34e2d470b

--- src/PlatformVersion.cmake
+++ src/PlatformVersion.cmake
@@ -42,7 +42,7 @@
 #
 SET(BrowserPlusPlatform_MAJOR_VERSION 2)
 SET(BrowserPlusPlatform_MINOR_VERSION 9)
-SET(BrowserPlusPlatform_MICRO_VERSION 0)
+SET(BrowserPlusPlatform_MICRO_VERSION 1)
 
 # Previous mimetypes which we also support as a CMAKE list
 # Syntax is:
@@ -53,7 +53,7 @@ SET(BackwardCompatibleMimeTypes)
 
 # activeX control's clsid and typelib guid
 # clsid (ActiveXGuid) should be changed EVERY TIME YOU BUMP PLATFORM VERSION
-SET(ActiveXGuid "A459E66E-7472-4353-A01C-A8234EDE8B66")
+SET(ActiveXGuid "36B2872D-6DFE-11DF-9038-001422210E8C")
 SET(TypeLibGuid "399F26B4-E0C6-4345-8AD6-7AC1D86DAAA5") 
 
 ########################################################################
