84c18806e6aa4d50f2bfddb84151e3e9cdce16a1
Gordon Durand <gordon_durand@yahoo.com>
2011-04-12 18:48:35 -0700

    bump to 2.10.15

--- src/PlatformVersion.cmake
+++ src/PlatformVersion.cmake
@@ -42,7 +42,7 @@
 #
 SET(BrowserPlusPlatform_MAJOR_VERSION 2)
 SET(BrowserPlusPlatform_MINOR_VERSION 10)
-SET(BrowserPlusPlatform_MICRO_VERSION 14)
+SET(BrowserPlusPlatform_MICRO_VERSION 15)
 
 # Previous mimetypes which we also support as a CMAKE list
 # Syntax is:
@@ -53,7 +53,7 @@ SET(BackwardCompatibleMimeTypes)
 
 # activeX control's clsid and typelib guid
 # clsid (ActiveXGuid) should be changed EVERY TIME YOU BUMP PLATFORM VERSION
-SET(ActiveXGuid "D5FA7111-D9C8-432E-90E9-A0815ADE4E1E")
+SET(ActiveXGuid "D83F7ED9-4BD4-475F-A7C1-F95016195477")
 SET(TypeLibGuid "399F26B4-E0C6-4345-8AD6-7AC1D86DAAA5") 
 
 # guid for control panel item on vista and later
